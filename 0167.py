77f6cd1f608b9c99b09a19fe4b2a92946a76b279
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-01-30 10:50:20 +0530

    Leetcode 0167: Two Sum II - Array is sorted

---
+++ 0167.py
@@ -0,0 +1,81 @@
+"""
+167. Two Sum II - Input Array Is Sorted
+
+Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, 
+find two numbers such that they add up to a specific target number. Let these two numbers be 
+numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+
+Return the indices of the two numbers, index1 and index2, 
+added by one as an integer array [index1, index2] of length 2.
+
+The tests are generated such that there is exactly one solution. 
+You may not use the same element twice
+
+Your solution must use only constant extra space.
+
+Example 1:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+Example 2:
+
+Input: numbers = [2,3,4], target = 6
+Output: [1,3]
+Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+Example 3:
+
+Input: numbers = [-1,0], target = -1
+Output: [1,2]
+Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+"""
+import time
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(numbers) - 1
+
+        while True:
+            sum = numbers[left] + numbers[right]
+            if sum > target:
+                right -= 1
+            elif sum < target:
+                left += 1
+            else:
+                return [left+1, right+1]
+            if left >= right:
+                break
+        return
+
+    def twoSumHashMap(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+
+        """
+        hash_map = dict()
+        for index, value in enumerate(numbers):
+            diff = target - value
+            if diff in hash_map:
+                return [hash_map[diff] + 1, index + 1]
+            hash_map[value] = index
+        return
+
+solution = Solution()
+numbers = [2,4,7,8,10,11]
+target = 13
+start = time.time()
+print(solution.twoSum(numbers, target))
+end = time.time()
+print("Execution time: {0} ms".format((end-start) * 10**3))
+start = time.time()
+print(solution.twoSumHashMap(numbers, target))
+end = time.time()
+print("Execution time: {0} ms".format((end-start) * 10**3))

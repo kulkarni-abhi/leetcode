7d815cdbfec5a05404b25477d4f79f6ab2019cbc
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-05-16 16:22:40 +0530

    Leetcode 006: Zigzag Conversion String Matrix
    Leetcode 006: Zigzag Conversion String Matrix

--- 0006.py
+++ 0006.py
@@ -1,27 +1,72 @@
-  """
-  Approach
-  --------
-  If our row is less than the numRows then we can increase it,
-  so that the next character can be placed at the next row,
-  but if the row is equal to the numRows - 1 then
-  we should change direction and place the characters
-  in the previous rows.
-  """
-  row = -1
-  direction = "positive"
-  rows = [""] * numRows
+"""
+6. Zigzag Conversion
 
-  for char in s:
-    if direction is "positive":
-      row += 1
-    else:
-      row = -1
+The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
 
-    rows[row] += char
-    if row < 1:
-      direction = "positive"
-    if row == numRows - 1:
-      direction = "negative"
-  return "".join(rows)
+P   A   H   N
+A P L S I I G
+Y   I   R
+And then read line by line: "PAHNAPLSIIGYIR"
 
-print(zigzag("PAYPALISHIRING", 4))
+Write the code that will take a string and make this conversion given a number of rows:
+
+string convert(string s, int numRows);
+
+
+Example 1:
+
+Input: s = "PAYPALISHIRING", numRows = 3
+Output: "PAHNAPLSIIGYIR"
+Example 2:
+
+Input: s = "PAYPALISHIRING", numRows = 4
+Output: "PINALSIGYAHRPI"
+Explanation:
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+Example 3:
+
+Input: s = "A", numRows = 1
+Output: "A"
+
+
+Constraints:
+
+1 <= s.length <= 1000
+s consists of English letters (lower-case and upper-case), ',' and '.'.
+1 <= numRows <= 1000
+
+"""
+
+class Solution(object):
+  def convert(self, s, numRows):
+    """
+    Approach
+    --------
+    If our row is less than the numRows then we can increase it,
+    so that the next character can be placed at the next row,
+    but if the row is equal to the numRows - 1 then
+    we should change direction and place the characters
+    in the previous rows.
+    """
+    row = -1
+    direction = "positive"
+    rows = [""] * numRows
+
+    for char in s:
+      if direction is "positive":
+        row += 1
+      else:
+        row = -1
+
+      rows[row] += char
+      if row < 1:
+        direction = "positive"
+      if row == numRows - 1:
+        direction = "negative"
+    return "".join(rows)
+
+solution = Solution()
+print(solution.convert("PAYPALISHIRING", 4))

22aba6c359ad42a7d156dd0ba346d6896a4896d6
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-08 02:09:00 +0530

    Leetcode 0058: Length of last word in string
    Leetcode 0058: Length of last word in string

---
+++ 0058.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        
+        count = 0
+        for i in range(len(s)-1, -1, -1):
+            if count == 0 and s[i] == " ":
+                continue
+            if s[i] == " ":
+                break
+            count += 1
+        return count

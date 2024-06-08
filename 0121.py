99e1d8360bb025b18614bd22db665ca9aab45dd7
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-08 23:42:58 +0530

    Leetcode 0121: Best Time to Buy and Sell Stock
    Leetcode 0121: Best Time to Buy and Sell Stock

---
+++ 0121.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+
+
+        1. min = prices[0]
+        2. max = 0
+        3. 
+        """
+        maximum = 0
+        minimum = prices[0]
+
+        for price in prices:
+            if price < minimum:
+                minimum = price
+            elif price - minimum > maximum:
+                maximum = max(price, maximum)
+        return maximum

097479def8be910ba75a9bee565c031bf042a732
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-09 00:36:01 +0530

    Leetcode 026: Remove Duplicates from Sorted Array
    Leetcode 026: Remove Duplicates from Sorted Array

--- 0026.py
+++ 0026.py
@@ -1,5 +1,34 @@
 class Solution(object):
     def removeDuplicates(self, nums):
+        if not nums:
+            return 0
+        
+        k = 1  # Initialize the count of unique elements to 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[i - 1]:
+                nums[k] = nums[i]  # Overwrite the next unique element
+                k += 1
+        return k
+
+    def removeDuplicates_hash(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        i = 0
+        hash_map = dict()
+        while True:
+            if i < len(nums):
+                if nums[i] in hash_map:
+                    del nums[i]
+                    continue
+                else:
+                    hash_map[nums[i]] = 1
+            else:
+                break
+            i += 1
+            
+    def removeDuplicates_no_recommended(self, nums):
         """
         :type nums: List[int]
         :rtype: int

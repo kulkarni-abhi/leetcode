27e2a81cf3fa02c1bbc8636f169887eb37eba346
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-08 18:52:35 +0530

    Leetcode 0066: Add one to list
    Leetcode 0066: Add one to list

---
+++ 0066.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        result = list()
+        carry = 0
+        for i in range(len(digits)-1, -1, -1):
+            num = 0
+            if i == len(digits) - 1:
+                num = 1
+            total = digits[i] + num + carry
+            val = total % 10
+            carry = total / 10
+            result.insert(0, val)
+        if carry:
+            result.insert(0, carry)
+        return result

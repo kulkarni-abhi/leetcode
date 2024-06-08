738e5b9767fbda8fd2460de49134644014507f9e
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-09 01:17:49 +0530

    Leetcode 0088: Merge two sorted lists
    Leetcode 0088: Merge two sorted lists

---
+++ 0088.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+         # Initialize nums1's index
+        i = m - 1
+        # Initialize nums2's index
+        j = n - 1
+        # Initialize a variable k to store the last index of the 1st array...
+        k = m + n - 1
+        while j >= 0:
+            if i >= 0 and nums1[i] > nums2[j]:
+                nums1[k] = nums1[i]
+                i -= 1
+            else:
+                nums1[k] = nums2[j]
+                j -= 1
+            k -= 1

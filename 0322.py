ee3dfabbdef6b1b6ab3577d523f47d004f8f2f78
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-10 01:53:36 +0530

    Leetcode 0322: Coin Change - Smallest Change
    Leetcode 0322: Coin Change - Smallest Change

--- 0322.py
+++ 0322.py
@@ -5,6 +5,27 @@ Tabular Solution
 
 """
 class Solution(object):
+    def coinChangeTabularRecommended(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        max_value = amount + 1
+        dp = [max_value] * (amount + 1)
+        dp[0] = 0
+       
+        for coin in coins:
+            for x in range(coin, amount + 1):
+               dp[x] = min(dp[x], dp[x - coin] + 1)
+    
+    
+        if dp[amount] != max_value:
+            return dp[amount]
+        else:
+            return -1
+
+    
     def coinChange(self, coins, amount):
         """
         :type coins: List[int]

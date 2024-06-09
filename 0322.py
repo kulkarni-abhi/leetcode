88deafd0e6c49413b690ac2b577030cb4cd65919
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-09 19:54:04 +0530

    Leetcode 0322: Coin Change
    Leetcode 0322: Coin Change

---
+++ 0322.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def coinChange(self, coins, amount):
+        """
+        :type coins: List[int]
+        :type amount: int
+        :rtype: int
+        """
+        dp = [0 for _ in range(amount + 1)]
+        for amt in range(1, amount + 1):
+            res = float('inf')
+            for denom in coins:
+                if denom <= amt:
+                    res = min(res, dp[amt - denom])
+            dp[amt] = res + 1
+        if dp[-1] == float('inf'):
+            return -1
+        return dp[-1] 

04352c4c2adba594161c31a2b09e197b7f3f2bad
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-11 22:35:48 +0530

    Leetcode 0028: First Occurrence of String
    Leetcode 0028: First Occurrence of String

--- 0028.py
+++ 0028.py
@@ -14,5 +14,18 @@ class Solution(object):
                 break
         return position
 
+    def strStr1(self, haystack: str, needle: str) -> int:
+        n = len(needle)
+        for i in range(len(haystack) - n + 1):
+            matched = True
+            ## If array slicing not allowed.
+            ## Match each character of needle with haystack
+            for j in range(n):
+                if needle[j] != haystack[i+j]:
+                    matched = False
+                    break
+            if matched:
+                return i
+        return -1
 solution = Solution()
 print(solution.strStr("hello", "ll"))

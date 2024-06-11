b95162fcc66f36ca93cd7c4f6a98463bc8bb4538
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-11 23:22:09 +0530

    Leetcode 0058: Length of last word in string
    Leetcode 0058: Length of last word in string

--- 0058.py
+++ 0058.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        
+
+        # Start pointer in reverse
+        # because we want to find the last word
+        # so lets not start from beginning to traverse to the last word
+        #
+        # then, ignore if there are whitespaces at the end,
+        # counting of the last word is not yet started
+        # once we receive the last character of the last word start counting
+        # move pointer to left until we get whitespace at the beginning of last word.
+        # 
         count = 0
         for i in range(len(s)-1, -1, -1):
             if count == 0 and s[i] == " ":

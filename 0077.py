19a7b8a282e530da9ab0476b7abc8c0838bdcf78
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-06-12 12:03:12 +0530

    Leetcode 0077: Combinations of K numbers within range 1 to n
    Leetcode 0077: Combinations of K numbers within range 1 to n

--- 0077.py
+++ 0077.py
@@ -14,3 +14,29 @@ class Solution:
         # so not starting from 0, and upto n+1 so that n is also included.
         dfs(1, [])
         return result
+
+"""
+Design Logic :--
+e.g. 
+Find all combinations of 3 elements from the list of 5 elements
+mylist = [A, B, C, D, E]
+R = 3
+n = 5                      
+
+                                                       [ ]
+                                                        |
+                                                        |
+                                ,------------------------------,------------,
+                                |                              |            |
+                                |                              |            |
+                                A                              B            C     <-------- Level #1 (Pick 1 element in sequence until r = 3)
+                                |                              |            | 
+                                |                              |            |
+                        ,-------------,----------,        ,---------,       |
+                        |             |          |        |         |       |
+                        AB           AC          AD       BC        BD      CD    <-------- Level #2 (Pick 2 elements in sequence until r = 3)
+                        |             |          |        |         |       |
+                     ,---,---,     ,-----,       |    ,-------,     |       |
+                     |   |   |     |     |       |    |       |     |       |
+                    ABC ABD ABE   ACD  ACE      ADE  BCD     BCE   BDE     CDE    <-------- Level #3 (Pick 3 elements in sequence until r = 3)
+"""

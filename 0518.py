1f39586ff337b1ac970a348960813171cd1d5c52
kulkarni-abhi <49950191+kulkarni-abhi@users.noreply.github.com>
2024-08-05 02:31:07 +0530

    Leetcode 0518: Coin change II - Number of combinations
    Leetcode 0518: Coin change II - Number of combinations

---
+++ 0518.py
@@ -0,0 +1,20 @@
+"""
+Leetcode 0518: Coin change - II
+"""
+
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp = [[0] * (amount+1) for _ in range(len(coins))]
+        for i in range(len(coins)):
+            for j in range(amount + 1):
+                if j == 0:
+                    dp[i][j] = 1
+                    continue
+                if coins[i] > j:
+                    dp[i][j] = dp[i-1][j]
+                else:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-coins[i]]
+        #pp.pprint(dp)
+        return dp[-1][-1]
